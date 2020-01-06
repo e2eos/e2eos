f70673333c66897de3ec71c60b86642cf0939c92
di.d.qu <di.d.qu@accenture.com>
2020-01-06 21:08:33 +0800

    feat(e2eos-angular-antd): Add input

--- packages/e2eos-angular-antd/src/app/app-routing.module.ts
+++ packages/e2eos-angular-antd/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ButtonPageComponent } from './pages/button-page/button-page.component';
 import { IconPageComponent } from './pages/icon-page/icon-page.component';
+import { InputPageComponent } from './pages/input-page/input-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'button' },
   { path: 'button', component: ButtonPageComponent },
   { path: 'icon', component: IconPageComponent },
+  { path: 'input', component: InputPageComponent },
 ];
 
 @NgModule({
