b8b3324a07e2df6b3fa8c7abd4a3ff3d4aeadc48
yifeng.li <yifeng.li@accenture.com>
2020-01-10 15:10:48 +0800

    'feat/(e2eos-angular-antd/collapse):collapse'

--- packages/e2eos-angular-antd/src/app/components/collapse-panel/collapse-panel.component.ts
+++ packages/e2eos-angular-antd/src/app/components/collapse-panel/collapse-panel.component.ts
@@ -1,13 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {AstNode, ComponentBase, NodeConfig} from 'e2eos-common/index';
+
+export const TAG_NAME = 'e2eos-ng-antd-collapse-panel';
+
+export const NODE_CONFIG: NodeConfig = {
+  name: TAG_NAME,
+  values: Object.freeze({
+    nzAccordion: false,
+    nzBordered: true
+  }),
+  attrs: [
+    {
+      key: 'nzAccordion',
+      type: 'boolean',
+      name: 'nzAccordion',
+      desc: '是否每次只打开一个tab'
+    },
+    {
+      key: 'nzBordered',
+      type: 'boolean',
+      name: 'nzBordered',
+      desc: '是否有边框'
+    }
+  ],
+  children: []
+};
+
+export interface AstCollapsePanelAttrs {
+  nzAccordion?: boolean;
+  nzBordered?: boolean;
+}
 
 @Component({
   selector: 'e2eos-ng-antd-collapse-panel',
   templateUrl: './collapse-panel.component.html',
   styleUrls: ['./collapse-panel.component.scss']
 })
-export class CollapsePanelComponent implements OnInit {
+export class CollapsePanelComponent implements ComponentBase, OnInit {
+  @Input() node: AstNode = {
+    tag: TAG_NAME,
+    attrs: NODE_CONFIG.attrs,
+    children: NODE_CONFIG.children
+  };
 
-  constructor() { }
+  get attrs(): AstCollapsePanelAttrs {
+    return this.node.attrs || NODE_CONFIG.attrs;
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- packages/e2eos-angular-antd/src/app/components/collapse/collapse.component.ts
+++ packages/e2eos-angular-antd/src/app/components/collapse/collapse.component.ts
@@ -1,13 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {AstNode, ComponentBase, NodeConfig} from 'e2eos-common/index';
+
+
+export const TAG_NAME = 'e2eos-ng-antd-collapse';
+
+export const NODE_CONFIG: NodeConfig = {
+  name: TAG_NAME,
+  values: Object.freeze({
+    nzAccordion: false,
+    nzBordered: true
+  }),
+  attrs: [
+    {
+      key: 'nzAccordion',
+      type: 'boolean',
+      name: 'nzAccordion',
+      desc: '是否每次只打开一个tab'
+    },
+    {
+      key: 'nzBordered',
+      type: 'boolean',
+      name: 'nzBordered',
+      desc: '是否有边框'
+    }
+  ],
+  children: []
+};
+
+export interface AstCollapseAttrs {
+  nzAccordion?: boolean;
+  nzBordered?: boolean;
+}
 
 @Component({
   selector: 'e2eos-ng-antd-collapse',
   templateUrl: './collapse.component.html',
   styleUrls: ['./collapse.component.scss']
 })
-export class CollapseComponent implements OnInit {
+export class CollapseComponent implements ComponentBase, OnInit {
+  @Input() node: AstNode = {
+    tag: TAG_NAME,
+    attrs: NODE_CONFIG.attrs,
+    children: NODE_CONFIG.children
+  };
 
-  constructor() { }
+  get attrs(): AstCollapseAttrs {
+    return this.node.attrs || NODE_CONFIG.attrs;
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
