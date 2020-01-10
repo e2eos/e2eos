f1fdaef747c73b9452c77bd25a7093490b2db8b5
yifeng.li <yifeng.li@accenture.com>
2020-01-10 13:43:11 +0800

    'feat/(e2eos-angular-antd/collapse):collapse'

---
+++ packages/e2eos-angular-antd/src/app/components/collapse-panel/collapse-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'e2eos-ng-antd-collapse-panel',
+  templateUrl: './collapse-panel.component.html',
+  styleUrls: ['./collapse-panel.component.scss']
+})
+export class CollapsePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
