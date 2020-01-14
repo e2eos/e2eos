77b56df35c01d1a3e9418d1a0b5cb2be8828b402
yifeng.li <yifeng.li@accenture.com>
2020-01-14 13:37:34 +0800

    'feat/(e2eos-angular-antd/collapse):collapse与collapse-panel组合组件'

--- packages/e2eos-angular-antd/src/app/components/collapse/collapse.component.ts
+++ packages/e2eos-angular-antd/src/app/components/collapse/collapse.component.ts
@@ -4,69 +4,27 @@ import {AstNode, ComponentBase, NodeConfig} from 'e2eos-common/index';
 
 export const TAG_NAME = 'e2eos-ng-antd-collapse';
 export const CHILD_TAG_NAME = 'e2eos-ng-antd-collapse-panel';
-
-export const CHILD_NODE_CONFIG: NodeConfig = {
-  name: CHILD_TAG_NAME,
-  values: Object.freeze({
+const collapsePanelJason: AstNode = {
+  tag: CHILD_TAG_NAME,
+  attrs: {
     nzDisabled: false,
-    nzHeader: '',
-    nzExpandedIcon: '',
+    nzHeader: '标题1',
+    nzExpandedIcon: 'layout',
     nzExtra: '',
     nzShowArrow: true,
     nzActive: true
-  }),
-  attrs: [
-    {
-      key: 'nzDisabled',
-      type: 'boolean',
-      name: 'nzDisabled',
-      desc: '禁用后的面板展开与否将无法通过用户交互改变'
-    },
-    {
-      key: 'nzHeader',
-      type: 'string',
-      name: 'nzHeader',
-      desc: '面板头内容'
-    },
-    {
-      key: 'nzExpandedIcon',
-      type: 'string',
-      name: 'nzExpandedIcon',
-      desc: '自定义切换图标'
-    },
-    {
-      key: 'nzExtra',
-      type: 'string',
-      name: 'nzExtra',
-      desc: '自定义渲染每个面板右上角的内容'
-    },
-    {
-      key: 'nzShowArrow',
-      type: 'boolean',
-      name: 'nzShowArrow',
-      desc: '是否展示箭头'
-    },
-    {
-      key: 'nzActive',
-      type: 'boolean',
-      name: 'nzActive',
-      desc: '面板头内容'
-    }
-  ],
+  },
   children: []
 };
 
-export const node1 = {
-  tag: CHILD_TAG_NAME,
-  attrs: CHILD_NODE_CONFIG.attrs,
-  children: CHILD_NODE_CONFIG.children
-};
+
 
 export const NODE_CONFIG: NodeConfig = {
   name: TAG_NAME,
   values: Object.freeze({
     nzAccordion: false,
-    nzBordered: true
+    nzBordered: true,
+    nzCollapsePanels: [collapsePanelJason]
   }),
   attrs: [
     {
@@ -80,23 +38,22 @@ export const NODE_CONFIG: NodeConfig = {
       type: 'boolean',
       name: 'nzBordered',
       desc: '是否有边框'
-    }
+    },
+    // {
+    //   key: 'nzCollapsePanels',
+    //   type: 'string',
+    //   name: 'nzCollapsePanels',
+    //   desc: 'collapsePanels数组集合'
+    // }
   ],
-  children: [node1]
+  children: []
 };
 
-export interface AstCollapsePanelAttrs {
-  nzDisabled?: boolean;
-  nzHeader?: string;
-  nzExpandedIcon?: string;
-  nzExtra?: string;
-  nzShowArrow?: boolean;
-  nzActive?: boolean;
-}
 
 export interface AstCollapseAttrs {
   nzAccordion?: boolean;
   nzBordered?: boolean;
+  nzCollapsePanels?: AstNode[];
 }
 
 @Component({
@@ -111,18 +68,9 @@ export class CollapseComponent implements ComponentBase, OnInit {
     children: NODE_CONFIG.children
   };
 
-  @Input() node1: AstNode = {
-    tag: CHILD_TAG_NAME,
-    attrs: CHILD_NODE_CONFIG.attrs,
-    children: CHILD_NODE_CONFIG.children
-  };
 
   get attrs(): AstCollapseAttrs {
     return this.node.attrs || NODE_CONFIG.attrs;
-  }
-
-  get panelAttrs(): AstCollapsePanelAttrs {
-    return this.node1.attrs || CHILD_NODE_CONFIG.attrs;
   }
 
   @Output() nzActiveChange = new EventEmitter();
